68b8ce7385e597b618019a67901f030d3b4588bc
rafaelarthurov <rafael.arthurov@gmail.com>
2022-06-12 19:06:50 -0400

    git

--- SuffleName.js
+++ SuffleName.js
@@ -9,3 +9,4 @@ function nameShuffler(str){
 console.log( nameShuffler('john McClane') ); //         Expected result: McClane john
 console.log( nameShuffler('Arturo Valdez') ); //        Expected result: Valdez Arturo
 console.log( nameShuffler('Sael Valdez') ); //          Expected result: Valdez Sael
+console.log( nameShuffler('Melissa Disla') ); //        Expected result: Disla Melissa
